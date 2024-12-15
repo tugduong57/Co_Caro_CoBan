00519f08c3f175e15e259fe7c31c6261ee257faf
tugduong57 <tugduognguyen@gmail.com>
2024-12-15 14:32:18 +0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,178 +1,235 @@
-import pygame
-import sys
-import checkWin
-
+import pygame, sys, checkWin
+# import os
+# os.chdir("E:/ThisPC/Desktop/Code/Caro")
 # Khởi tạo Pygame
 pygame.init()
-
 # Số lượng ô vuông trên bàn cờ 
 GRID_SIZE = 15; 
-nxn = GRID_SIZE; banco = []; NumberToWin = 5; player = ""
-for i in range(nxn):
-    banco.append([])
-    for j in range(nxn):
-        banco[i].append('.')
-# Lấy kích thước màn hình
-info = pygame.display.Info()
-screen_width = info.current_w
-screen_height = info.current_h
-# print(screen_width, screen_height)
-T = {} # 
-
-def addT (value, x, y):
-    if value not in T:
-        T[value] = []
-    T[value].append((x,y))
-
-def checkR(value, x, y):
-    if value in T:
-        T[value].remove( (x, y))
-
+# Cách biến toàn cục
+nxn = GRID_SIZE; banco = []; NumberToWin = 5; player = ""; AI = ""; turn = ""
+banco = [ ['.']*nxn for _ in range(nxn)]
 
 def taoBangDanhGia(N):
     matrix = [[0] * N for _ in range(N)]
     num_layers = (N + 1) // 2
-
-    for layer in range(num_layers):
+    for layer in range(5,num_layers):
         value = layer + 1
         for i in range(layer, N - layer):
-            matrix[layer][i] = value
-            if value not in T:
-                T[value] = []
-            T[value].append((layer, i))
-        for i in range(layer, N - layer):
-            matrix[N - layer - 1][i] = value
-            if value not in T:
-                T[value] = []
-            T[value].append((N - layer - 1, i))
+            matrix[layer][i] = value; matrix[N - layer - 1][i] = value
         for i in range(layer + 1, N - layer - 1):
-            matrix[i][layer] = value
-            if value not in T:
-                T[value] = []
-            T[value].append((i, layer))
-        for i in range(layer + 1, N - layer - 1):
-            matrix[i][N - layer - 1] = value
-            if value not in T:
-                T[value] = []
-            T[value].append((i, N - layer - 1))
-
+            matrix[i][layer] = value; matrix[i][N - layer - 1] = value
     for i in range(N):
-        checkR(matrix[i][i], i, i); matrix[i][i] += 1; addT(matrix[i][i], i, i)
-        checkR(matrix[i][N - i - 1], i, N - i - 1); matrix[i][N - i - 1] += 1; addT(matrix[i][N - i - 1], i, N - i - 1)
+        matrix[i][i] += 1; matrix[i][N - i - 1] += 1
     return matrix
 
 def print_matrix(matrix):
     for row in matrix:
-        print(" ".join(f"{value:2}" for value in row))
+        print(" ".join(f"{value:5}" for value in row))
 
-Score = taoBangDanhGia(nxn); print_matrix(Score); #print(T)
+Score = taoBangDanhGia(nxn); print_matrix(Score); 
+Map = []  # 1 ma trận với mỗi vị trí nó lưu 1 giá trị tổng và 8 xâu cho 8 hướng
+# Map[i] -> [value, "North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"] 
+#                   5 -> 1; 6 -> 2; 7 -> 3; 8 -> 4
+for i in range(nxn):
+    Map.append( [] )
+    for j in range(nxn):
+        Map[i].append( [0] + ["00"]*8 + [0,0,0,0])
+        if i == 0:
+            Map[i][j][1] = "-1"
+        if j == nxn-1: 
+            Map[i][j][3] = "-1"
+        if i == nxn-1:
+            Map[i][j][5] = "-1"
+        if j == 0:
+            Map[i][j][7] = "-1"
 
-scoreMax = 0; List_scoreMax = []
+for i in range(nxn):
+    print(Map[i])
 
-# Kích thước của cửa sổ
-WINDOW_WIDTH = 800; WINDOW_HEIGHT = 800
-SQUARE_SIZE = min(WINDOW_WIDTH, WINDOW_HEIGHT) // GRID_SIZE # Tính toán kích thước mỗi ô vuông
-# Màu sắc
+# TH: bảng tra cứu điểm
+BangDiem = {}
+with open("BangDiem.txt", encoding="utf-8") as inp:
+    for i in range(34):
+        line = inp.readline().split(";")
+        BangDiem[line[0]] = int(line[1])
+TH = {}
+with open("G.txt", encoding="utf-8") as inp:
+    for i in range(361):
+        xau = "".join(inp.readline().split())
+        line = inp.readline().split(";")
+        TH[xau] = BangDiem[line[0]] + BangDiem[line[1]]
+        
+        
+Direc = {(-1, 0): 1, (-1, 1): 2, (0, 1): 3, (1, 1): 4, (1, 0): 5, (1, -1):6, (0, -1):7, (-1, -1):8}
+
+
+# T: dir lưu tọa độ các điểm có cùng value
+T = {} # 
+
+def AddT(value, x, y):
+    if value not in T:
+        T[value] = []
+    T[value].append((x,y))
+
+def RemoveT(value, x, y):
+    if value in T:
+        if (x,y) in T[value]:
+            T[value].remove((x, y))
+    if T[value] == []:
+        T.pop(value)
+
+for i in range(nxn):
+    for j in range(nxn):
+        value = Score[i][j]
+        AddT(value, i, j)       
+
+# Set up
+WINDOW_WIDTH = 800; WINDOW_HEIGHT = 800; SQUARE_SIZE = min(WINDOW_WIDTH, WINDOW_HEIGHT) // GRID_SIZE
 WHITE = (255, 255, 255); BACKGROUND_COLOR = (255, 255, 255); LINE_COLOR = (0, 0, 0); X_COLOR = (0, 0, 0); O_COLOR = (0, 0, 0)
+
 # Tạo cửa sổ
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Bàn cờ XO')
 # Hàm vẽ bàn cờ
 def draw_board():
     screen.fill(BACKGROUND_COLOR)
-    # Vẽ các đường kẻ
-    #      pygame.draw.circle(screen, WHITE, (x,y), r))
-    # -> vẽ hình tròn trên screen, màu, tâm có tọa độ (x,y), bán kính r
-
     for row in range(1, GRID_SIZE):
-        pygame.draw.line(screen, LINE_COLOR, (0, row * SQUARE_SIZE) , (WINDOW_WIDTH, row * SQUARE_SIZE)  , 3)
-        pygame.draw.line(screen, LINE_COLOR, (row * SQUARE_SIZE, 0) , (row * SQUARE_SIZE, WINDOW_HEIGHT) , 3)
-    pygame.draw.line(screen, LINE_COLOR, (0, 0 * SQUARE_SIZE), (WINDOW_WIDTH, 0* SQUARE_SIZE), 5); pygame.draw.line(screen, LINE_COLOR, (0 * SQUARE_SIZE, 0), (0 * SQUARE_SIZE, WINDOW_HEIGHT), 5)
-    pygame.draw.line(screen, LINE_COLOR, (0, GRID_SIZE * SQUARE_SIZE-2), (WINDOW_WIDTH, GRID_SIZE* SQUARE_SIZE-2), 5); pygame.draw.line(screen, LINE_COLOR, (GRID_SIZE * SQUARE_SIZE-1, 0), (GRID_SIZE * SQUARE_SIZE-1, WINDOW_HEIGHT), 5)
+        pygame.draw.line(screen, LINE_COLOR, (0, row * SQUARE_SIZE) , (WINDOW_WIDTH, row * SQUARE_SIZE)  , 3); pygame.draw.line(screen, LINE_COLOR, (row * SQUARE_SIZE, 0) , (row * SQUARE_SIZE, WINDOW_HEIGHT) , 3)
+    pygame.draw.line(screen, LINE_COLOR, (0, 0 * SQUARE_SIZE), (WINDOW_WIDTH, 0* SQUARE_SIZE), 5); pygame.draw.line(screen, LINE_COLOR, (0 * SQUARE_SIZE, 0), (0 * SQUARE_SIZE, WINDOW_HEIGHT), 5); pygame.draw.line(screen, LINE_COLOR, (0, GRID_SIZE * SQUARE_SIZE-2), (WINDOW_WIDTH, GRID_SIZE* SQUARE_SIZE-2), 5); pygame.draw.line(screen, LINE_COLOR, (GRID_SIZE * SQUARE_SIZE-1, 0), (GRID_SIZE * SQUARE_SIZE-1, WINDOW_HEIGHT), 5)
 # Hàm vẽ X và O
 def draw_XO(banco):
-	# liên tục phải vẽ X và O lên màn hình ==> vậy thì check luôn xem list các ô có điểm lớn nhất đang là những ô nào 
-    global scoreMax; global List_scoreMax
     for row in range(GRID_SIZE):
         for col in range(GRID_SIZE):
             if banco[row][col] == 'X':
-                pygame.draw.line(screen, X_COLOR, (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + 20), 
-                                 (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), 5)
-                pygame.draw.line(screen, X_COLOR, (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + 20), 
-                                 (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), 5)
-               	if (row,col) in List_scoreMax:
-               		List_scoreMax.remove( (row, col)); scoreMax = 0
+                pygame.draw.line(screen, X_COLOR, (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + 20), (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), 5); pygame.draw.line(screen, X_COLOR, (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + 20), (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), 5)
             elif banco[row][col] == 'O':
-                #pygame.draw.circle(screen, WHITE, (x,y), r))
-                pygame.draw.circle(screen, O_COLOR, 
-                                   (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 
-                                   SQUARE_SIZE // 2 - 7, 2) 
-                if (row,col) in List_scoreMax:
-               		List_scoreMax.remove( (row, col)); scoreMax = 0
-            else:
-            	if (row,col) not in List_scoreMax:
-	                t = Score[row][col]
-	                if t > scoreMax:
-	                    scoreMax = t
-	                    List_scoreMax = [(row,col)]
-	                elif t == scoreMax:
-	                    List_scoreMax.append( (row, col))
+                pygame.draw.circle(screen, O_COLOR, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), SQUARE_SIZE // 2 - 7, 2) 
 
-def update_score(matrix, player, x, y):
-    e = []
+def tinh_score(x, y,dx, dy):
+    if (dx, dy) in [ (1, 0), (1, -1), (0, -1), (-1, -1) ]:
+        xau = Map[x][y][Direc[(dx, dy)]] + "X" + Map[x][y][Direc[(-dx, -dy)]]   
+    elif (dx, dy) in [ (-1, 0),(-1, 1),  (0, 1), ( 1, 1 ) ]:
+        xau = Map[x][y][Direc[(-dx, -dy)]] +  "X" + Map[x][y][Direc[(dx, dy)]]
+    k = Direc[(dx, dy)]
+    if k > 4:
+        k -= 4
 
-    directions = [ # kim dong ho tu huong 12h
-        (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (-1, -1), (0, -1), (-1, -1)
-    ]
+    Map[x][y][8 + k] = TH[xau]
+    oldScore = Score[x][y]
+    #RemoveT(Score[x][y], x, y); Score[x][y] -= Map[x][y][0]
     
-    # Duyệt qua các hướng và kiểm tra các ô xung quanh
-    for dx, dy in directions:
-        for i in range(1, 3):  # Kiểm tra 2 ô ở mỗi hướng
-            new_x = x + dx * i; 	new_y = y + dy * i
-            # Kiểm tra nếu (new_x, new_y) là tọa độ hợp lệ trong ma trận
-            if 0 <= new_x < nxn and 0 <= new_y < nxn:
-                e.append( (banco[new_x][new_y], new_x, new_y) )
+    Map[x][y][0] = Map[x][y][9] + Map[x][y][10] + Map[x][y][11] + Map[x][y][12]
+    newScore = oldScore + Map[x][y][0];
+    
+    RemoveT(Score[x][y], x, y); Score[x][y] = newScore; AddT(Score[x][y], x, y)
+
+    #Score[x][y] += Map[x][y][0]; AddT(Score[x][y], x, y)
+
+# Hàm tính toán bảng đánh giá
+def update_score(turn, matrix, x, y, dx, dy):
+    # print("Check ")
+    canhke = []
+    trang = 1; i = 1
+    while True:
+        new_x = x + dx*i;     new_y = y + dy*i
+        if 0 <= new_x < nxn and 0 <= new_y < nxn:
+            giatri = banco[new_x][new_y]
+            print(giatri)
+            if giatri == turn:
+                canhke.append('1') # nếu cùng loại, xét tiếp
+                if trang != 0:
+                    trang = 0
+            elif giatri == ".":
+                if trang == 1: # nếu là ô trống đầu tiên
+                    canhke.append("0")
+                    trang -= 1
+                elif trang == 0:
+                    canhke.append("0")
+                    trang -= 1
+                if trang == -1:
+                    break
             else:
-                e.append(None)  # Nếu ra ngoài ma trận, thêm None
-    print(e)
+                canhke.append('-1'); 
+                break # Nếu không cùng loại -> dừng
+        else:
+            canhke.append("-1"); 
+            break
+        i += 1
+    print(Direc[(dx, dy)], canhke)
+    if (dx, dy) in [ (1, 0), (1, -1), (0, -1), (-1, -1) ]:
+        Map[x][y][Direc[(dx, dy)]] = "".join(canhke[::-1])       #xau = Map[x][y][Direc[(dx, dy)]] + "X" + Map[x][y][Direc[(-dx, -dy)]]    
+    elif (dx, dy) in [ (-1, 0),(-1, 1),  (0, 1), ( 1, 1 ) ]:
+        Map[x][y][Direc[(dx, dy)]] = "".join(canhke)            # print(Map[x][y][Direc[(-dx, -dy)]], "X", Map[x][y][Direc[(dx, dy)]])
 
+    tinh_score(x,y,dx,dy)
 
-# Hàm quản lý sự kiện chơi cờ
+def scan_score(matrix, turn, x, y):
+    directions = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    #               "North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"
+    #                5 -> 1: ; 6 -> 2; 7 -> 3; 8 -> 4
+    for dx, dy in directions: ## lan tỏa và check
+        h = []; trang = 1; i = 1
+        while True:
+            new_x = x + dx * i; 	new_y = y + dy * i
+            if 0 <= new_x < nxn and 0 <= new_y < nxn:
+                giatri = banco[new_x][new_y]
+                if giatri == turn: # nếu cùng loại, xét tiếp
+                    if trang != 1:
+                        trang -= 1
+                if giatri == ".":
+                    #print(new_x,new_y,turn,dx,dy)
+                    trang -= 1;
+                    update_score(turn, matrix, new_x, new_y, -dx, -dy)
+                    if trang == -1:
+                        break
+                if giatri != turn and giatri != ".":
+                    break # Nếu không cùng loại -> dừng
+            else:
+                break # Ra ngoài bàn cờ
+            i += 1
+
 def game_loop():
-    global player; global scoreMax
-    player = 'X'  # Bắt đầu với X
+    global player; global AI; global turn; player = 'X'; AI = 'O'; turn = player
     game_over = False
 
     while True:
-        draw_board()
-        draw_XO(banco)
-
+        draw_board(); draw_XO(banco)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
-                x_mouse, y_mouse = pygame.mouse.get_pos()
-                row, col = y_mouse // SQUARE_SIZE, x_mouse // SQUARE_SIZE
+                x_mouse, y_mouse = pygame.mouse.get_pos(); row, col = y_mouse // SQUARE_SIZE, x_mouse // SQUARE_SIZE
                 if banco[row][col] == '.':
-                    banco[row][col] = player; Score[row][col] = -1000
-                    # print(scoreMax, List_scoreMax)
-                    update_score(Score, player, row, col)
+
+                    banco[row][col] = player; 
+                    RemoveT(Score[row][col], row, col)
+                    Score[row][col] = -1000
                     if checkWin.checkWin(banco, player, row+1, col+1, nxn, NumberToWin) == "Win":
-                        game_over = True
+                        game_over = True; 
                         break
-                    if player == "X":
-                        player = "O"
-                    else:
-                        player = "X"
+                    scan_score(Score, player, row, col)
+                    print_matrix(Score); 
+
+                    (x, y) = T[max(T.keys())][0] # @@@@@
+
+                    banco[x][y] = AI; 
+                    draw_board();
+                    RemoveT(Score[x][y], x, y)
+                    Score[x][y] = -1000
+                    if checkWin.checkWin(banco, AI, x+1, y+1, nxn, NumberToWin) == "Win":
+                        game_over = True; 
+                        break
+                    scan_score(Score, AI, x, y)     # check tấn công
+                    scan_score(Score, player, x, y) # check phòng thủ
+                    print_matrix(Score); 
+                    
 
         if game_over:
             font = pygame.font.Font(None, 74)
-            winner_text = font.render(f'{player} wins!', True, (0, 0, 0))
+            winner_text = font.render(f'{turn} wins!', True, (0, 0, 0))
             screen.blit(winner_text, (WINDOW_WIDTH // 2 - winner_text.get_width() // 2, WINDOW_HEIGHT // 2 - winner_text.get_height() // 2))
-
         pygame.display.flip()
 
-# Bắt đầu trò chơi
 game_loop()
